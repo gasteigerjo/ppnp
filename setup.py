04675b7d67b053a96608016dd7721f0defdbbb41
Johannes Gasteiger, né Klicpera <gasteiger@mailbox.org>
2022-04-05 13:00:11 +0200

    Update last name outside readme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='ppnp',
       version='0.1',
       description='Predict then Propagate: Graph Neural Networks meet Personalized PageRank',
-      author='Johannes Klicpera, Aleksandar Bojchevski, Stephan Günnemann',
-      author_email='klicpera@in.tum.de',
+      author='Johannes Gasteiger, Aleksandar Bojchevski, Stephan Günnemann',
+      author_email='j.gasteiger@in.tum.de',
       packages=['ppnp'],
       zip_safe=False)
